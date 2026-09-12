7198ac7cb1d1bc58c48c9fc1e34958c646fab9ba
Kiruthika Chan <kiruthikachan3@gmail.com>
2026-09-11 21:00:11 -0400

    Updated quiz API routes for consistency

--- backend/learning/urls.py
+++ backend/learning/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("letters/", letter_list, name="letter-list"),
     path("lessons/", lesson_list, name="lesson-list"),
     path("lessons/<int:lesson_id>/", lesson_detail, name = "lesson-detail"),
-    path("lessons/<int:lesson_id>/quiz-attempts", create_quiz_attempt, name="create-quiz-attempt"),
-    path("quiz_attempts/<int:attempt_id>/submit", submit_quiz_attempt, name="submit-quiz-attempt"),
+    path("lessons/<int:lesson_id>/quiz-attempts/", create_quiz_attempt, name="create-quiz-attempt"),
+    path("quiz-attempts/<int:attempt_id>/submit/", submit_quiz_attempt, name="submit-quiz-attempt"),
 ]
